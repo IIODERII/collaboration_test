31c113bb35d3525a94db939708844b90adf04538
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-12-22 17:01:40 +0100

    upload web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/home', [App\Http\Controllers\TrainController::class , 'index'])->name('home');
